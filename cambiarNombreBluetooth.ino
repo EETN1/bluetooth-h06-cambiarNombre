e4df5867bd94c52ae58ae54e55739d330b705720
juanreichert <juanreichert@yahoo.com>
2022-11-07 11:54:42 -0300

    Update cambiarNombreBluetooth.ino
    Se agrego descripcion al codigo

--- cambiarNombreBluetooth.ino
+++ cambiarNombreBluetooth.ino
@@ -1,4 +1,6 @@
 //Utilizar para renombrar los modulos H06 Bluetooth
+//Tener en cuenta Cambiar en la consola Nueva Linea, la frecuencia en 9600 baudios
+
 #include <SoftwareSerial.h>
 SoftwareSerial BT1(10,11); // RX, TX recorder que se cruzan
 
@@ -6,6 +8,19 @@ void setup()
    {
        Serial.begin(9600);
        Serial.println("Ingresar comando AT:");
+         //Para cambiar el nombre deben escribir lo siguiente en la consola:
+         //AT+NAME=nombreDeMiBluetooth    
+         //Para cambiar la frecuencia ponemos el indice correspondiente en lugar de la x  AT+BAUDx
+         //  1 – 1200bps
+         // 2 – 2400bps
+         // 3 – 4800bps
+         // 4 – 9600bps (Default)
+         // 5 – 19200bps
+         // 6 – 38400bps
+         // 7 – 57600bps
+         // 8 – 115200bps   
+         //como por ejemplo AT+BAUD4 para fijarlo en 9600
+   
        BT1.begin(9600);
    }
 
